c7284c51e14d0bc59b2d05902f9c1a7cfe1d1865
Anton Kichaev <>
2023-02-27 10:37:35 +0700

    add contains int

--- utils.go
+++ utils.go
@@ -8,3 +8,12 @@ func Contains(key string, sl []string) bool {
 	}
 	return false
 }
+
+func ContainsInt(key int, sl []int) bool {
+	for _, el := range sl {
+		if el == key {
+			return true
+		}
+	}
+	return false
+}
